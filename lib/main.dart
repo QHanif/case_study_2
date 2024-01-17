fd3cb1fa804f51a5e8556fd22890542e47bfd226
Hanif <qoysjaafar@gmail.com>
2024-01-18 00:19:08 +0800

    rerouted login and register screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,41 @@ import 'dart:io';
 import 'package:case_study_2/widgettree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:case_study_2/pages/loginscreen.dart';
+import 'package:case_study_2/pages/registerscreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   Platform.isAndroid
-  ? await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      appId: '1:195063921079:android:a034ddc9d23f4275e0d032',
-      messagingSenderId: '195063921079',
-      projectId: 'fir-cs2',
-      apiKey: 'AIzaSyDAWq2sokNaM10ZUEAT91yNDQILA3pUa5Y',
-    ))
-    : await Firebase.initializeApp(); 
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          appId: '1:195063921079:android:a034ddc9d23f4275e0d032',
+          messagingSenderId: '195063921079',
+          projectId: 'fir-cs2',
+          apiKey: 'AIzaSyDAWq2sokNaM10ZUEAT91yNDQILA3pUa5Y',
+        ))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const WidgetTree(),
-    );  }
+      title: 'My App',
+      // initialRoute: '/',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+      },
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WidgetTree(),
+    );
+  }
 }
