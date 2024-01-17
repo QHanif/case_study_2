ba7dd4f0e65c26e51a8f96a2b0585eb0d4016f82
Konaya245 <mzaidi.ar@gmail.com>
2024-01-17 23:15:18 +0800

    added form widget, form key, validator and obscuretext
    googoo

--- lib/pages/loginregister.dart
+++ lib/pages/loginregister.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../auth.dart';
+import '../util/validate.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,68 +11,93 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final Validator validator = Validator();
   String? errorMessage = '';
   bool isLogin = true;
-
 
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
-  Future <void> signInWithEmailAndPassword() async {
+  Future<void> signInWithEmailAndPassword() async {
     try {
       await Auth().signInWithEmailAndPassword(
-        email : _controllerEmail.text,
-        password : _controllerPassword.text,
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
       );
     } on FirebaseAuthException catch (e) {
-      setState((){
-        errorMessage = e.message;
+      setState(() {
+        switch (e.code) {
+          case 'invalid-credential':
+            errorMessage = 'Invalid email/password entered. Please try again.';
+            break;
+          default:
+            errorMessage =
+                'An unknown error occurred with error code: ${e.code}.';
+        }
       });
     }
   }
 
-  Future <void> createUserWithEmailAndPassword() async {
-    try{
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
       await Auth().createUserWithEmailAndPassword(
-        email: _controllerEmail.text, 
+        email: _controllerEmail.text,
         password: _controllerPassword.text,
-        );
+      );
     } on FirebaseAuthException catch (e) {
-      setState((){
-        errorMessage = e.message;
+      setState(() {
+        switch (e.code) {
+          case 'email-already-in-use':
+            errorMessage =
+                'The account already exists for that email. Login instead.';
+            break;
+          default:
+            errorMessage =
+                'An unknown error occurred with error code: ${e.code}.';
+        }
       });
     }
   }
 
-  Widget _title(){
-    return Text('Firebase Auth');
+  Widget _title() {
+    return const Text('Firebase Auth');
   }
 
-  Widget _entryField(
-    String title,
-    TextEditingController controller,
-  ){
-    return TextField(
+  Widget _entryField(String title, TextEditingController controller,
+      String? Function(String?)? validator,
+      {bool obscureText = false}) {
+    return TextFormField(
       controller: controller,
+      obscureText: obscureText,
+      validator: validator,
       decoration: InputDecoration(
         labelText: title,
       ),
     );
   }
 
-  Widget _errorMessage(){
-    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage',
+        style: const TextStyle(color: Colors.red));
   }
 
-  Widget _submitButton(){
+  Widget _submitButton() {
     return ElevatedButton(
-      onPressed:
-      isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          // No validation errors. It's safe to proceed.
+          if (isLogin) {
+            signInWithEmailAndPassword();
+          } else {
+            createUserWithEmailAndPassword();
+          }
+        }
+      },
       child: Text(isLogin ? 'Login' : 'Register'),
     );
   }
 
-  Widget _loginOrRegisterButton(){
+  Widget _loginOrRegisterButton() {
     return TextButton(
       onPressed: () {
         setState(() {
@@ -82,28 +108,34 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _title(),
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget>[
-            _entryField('Email', _controllerEmail),
-            _entryField('Password', _controllerPassword),
-            _errorMessage(),
-            _submitButton(),
-            _loginOrRegisterButton(),
-          ],
+        appBar: AppBar(
+          title: _title(),
         ),
-      )
-    );
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _entryField('Email', _controllerEmail, validator.validateEmail),
+                _entryField(
+                    'Password', _controllerPassword, validator.validatePassword,
+                    obscureText: true),
+                _errorMessage(),
+                _submitButton(),
+                _loginOrRegisterButton(),
+              ],
+            ),
+          ),
+        ));
   }
 }
