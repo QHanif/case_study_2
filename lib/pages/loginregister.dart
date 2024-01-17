a8236959177e490f29d9f28f2b92b79e5b98cc64
Hanif <qoysjaafar@gmail.com>
2024-01-18 07:37:05 +0800

    Revert "Merge branch 'zaidi'"
    This reverts commit cdbfc224a702e29bf0fc93876da0be50ec6b58ac, reversing
    changes made to 21ff32455774c38b8f6083e07e547ed6f37597d3.

--- lib/pages/loginregister.dart
+++ lib/pages/loginregister.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../auth.dart';
-import '../util/validate.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,109 +10,68 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final Validator validator = Validator();
   String? errorMessage = '';
   bool isLogin = true;
+
 
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
-  Future<void> signInWithEmailAndPassword() async {
+  Future <void> signInWithEmailAndPassword() async {
     try {
       await Auth().signInWithEmailAndPassword(
-        email: _controllerEmail.text,
-        password: _controllerPassword.text,
+        email : _controllerEmail.text,
+        password : _controllerPassword.text,
       );
     } on FirebaseAuthException catch (e) {
-      setState(() {
-        switch (e.code) {
-          case 'invalid-credential':
-            errorMessage = 'Invalid email/password entered. Please try again.';
-            break;
-          default:
-            errorMessage =
-                'An unknown error occurred with error code: ${e.code}.';
-        }
+      setState((){
+        errorMessage = e.message;
       });
     }
   }
 
-  Future<void> createUserWithEmailAndPassword() async {
-    try {
+  Future <void> createUserWithEmailAndPassword() async {
+    try{
       await Auth().createUserWithEmailAndPassword(
-        email: _controllerEmail.text,
+        email: _controllerEmail.text, 
         password: _controllerPassword.text,
-      );
+        );
     } on FirebaseAuthException catch (e) {
-      setState(() {
-        switch (e.code) {
-          case 'email-already-in-use':
-            errorMessage =
-                'The account already exists for that email. Login instead.';
-            break;
-          default:
-            errorMessage =
-                'An unknown error occurred with error code: ${e.code}.';
-        }
+      setState((){
+        errorMessage = e.message;
       });
     }
   }
 
-  Widget _title() {
-    return const Text('Firebase Auth');
+  Widget _title(){
+    return Text('Firebase Auth');
   }
 
-  Widget _entryField(String title, TextEditingController controller,
-      String? Function(String?)? validator,
-      {bool isPassword = false}) {
-    bool obscureText = isPassword;
-    return StatefulBuilder(
-      builder: (BuildContext context, StateSetter setState) {
-        return TextFormField(
-          controller: controller,
-          obscureText: obscureText,
-          validator: validator,
-          decoration: InputDecoration(
-            labelText: title,
-            suffixIcon: isPassword
-                ? IconButton(
-                    icon: Icon(
-                        obscureText ? Icons.visibility_off : Icons.visibility),
-                    onPressed: () {
-                      setState(() {
-                        obscureText = !obscureText;
-                      });
-                    },
-                  )
-                : null,
-          ),
-        );
-      },
+  Widget _entryField(
+    String title,
+    TextEditingController controller,
+  ){
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
     );
   }
 
-  Widget _errorMessage() {
-    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage',
-        style: const TextStyle(color: Colors.red));
+  Widget _errorMessage(){
+    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
   }
 
-  Widget _submitButton() {
+  Widget _submitButton(){
     return ElevatedButton(
-      onPressed: () {
-        if (_formKey.currentState!.validate()) {
-          // No validation errors. It's safe to proceed.
-          if (isLogin) {
-            signInWithEmailAndPassword();
-          } else {
-            createUserWithEmailAndPassword();
-          }
-        }
-      },
+      onPressed:
+      isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
       child: Text(isLogin ? 'Login' : 'Register'),
     );
   }
 
-  Widget _loginOrRegisterButton() {
+  Widget _loginOrRegisterButton(){
     return TextButton(
       onPressed: () {
         setState(() {
@@ -124,34 +82,28 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  final _formKey = GlobalKey<FormState>();
-
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: _title(),
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget>[
+            _entryField('Email', _controllerEmail),
+            _entryField('Password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _loginOrRegisterButton(),
+          ],
         ),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _entryField('Email', _controllerEmail, validator.validateEmail),
-                _entryField(
-                    'Password', _controllerPassword, validator.validatePassword,
-                    isPassword: true),
-                _errorMessage(),
-                _submitButton(),
-                _loginOrRegisterButton(),
-              ],
-            ),
-          ),
-        ));
+      )
+    );
   }
 }
