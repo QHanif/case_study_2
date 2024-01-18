78330fa8e010699be1220a4a677d00b8232fa300
Hanif <qoysjaafar@gmail.com>
2024-01-18 09:14:36 +0800

    obsecure password

--- lib/pages/registerscreen.dart
+++ lib/pages/registerscreen.dart
@@ -11,8 +11,19 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   String? errorMessage = '';
+  final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
+  final TextEditingController _controllerConfirmPassword =
+      TextEditingController();
+
+  bool _isHiddenPassword = true;
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHiddenPassword = !_isHiddenPassword;
+    });
+  }
 
   Future<void> createUserWithEmailAndPassword() async {
     try {
@@ -51,7 +62,7 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Register'),
+        title: Text('Sign Up'),
       ),
       body: Container(
         height: double.infinity,
@@ -62,15 +73,56 @@ class _RegisterPageState extends State<RegisterPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
+              controller: _controllerUsername,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
               controller: _controllerEmail,
               decoration: InputDecoration(
                 labelText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
-            TextField(
+            SizedBox(height: 10.0),
+            TextFormField(
               controller: _controllerPassword,
+              obscureText: _isHiddenPassword,
               decoration: InputDecoration(
                 labelText: 'Password',
+                suffix: InkWell(
+                  onTap: _togglePasswordView,
+                  child: Icon(
+                    _isHiddenPassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              controller: _controllerConfirmPassword,
+              obscureText: _isHiddenPassword,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                suffix: InkWell(
+                  onTap: _togglePasswordView,
+                  child: Icon(
+                    _isHiddenPassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
             Text(errorMessage == '' ? '' : 'Hmm ? $errorMessage'),
@@ -89,4 +141,47 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
     );
   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Register'),
+//       ),
+//       body: Container(
+//         height: double.infinity,
+//         width: double.infinity,
+//         padding: EdgeInsets.all(20),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             TextField(
+//               controller: _controllerEmail,
+//               decoration: InputDecoration(
+//                 labelText: 'Email',
+//               ),
+//             ),
+//             TextField(
+//               controller: _controllerPassword,
+//               decoration: InputDecoration(
+//                 labelText: 'Password',
+//               ),
+//             ),
+//             Text(errorMessage == '' ? '' : 'Hmm ? $errorMessage'),
+//             ElevatedButton(
+//               onPressed: createUserWithEmailAndPassword,
+//               child: Text('Register'),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.pop(context);
+//               },
+//               child: Text('Go back to Login'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 }
